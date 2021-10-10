3d93edb1c3864f42b841d5a37a4995f47346842f
roomanalam <92167231+roomanalam@users.noreply.github.com>
2021-10-11 01:43:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from logging import error
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import random
@@ -6,7 +5,7 @@ import random
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../Day-66(first RESTful API)/cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
